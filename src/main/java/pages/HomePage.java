67b11a92956643cc087f9a19ad8a3121bd08dfe2
Saima <saimaalam.dsi@gmail.com>
2024-09-22 04:15:07 +0600

    init

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,78 +6,93 @@ import org.openqa.selenium.WebDriver;
 import java.util.concurrent.TimeUnit;
 
 public class HomePage {
-    private WebDriver driver;
-    public HomePage(WebDriver driver){
-        this.driver= driver;
+    private final WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
     }
-    private void clickLink(String linkText){
+
+    private void clickLink(String linkText) {
 
         driver.findElement(By.linkText(linkText)).click();
     }
-    public LoginPage clickFormAuthencation(){
+
+    public LoginPage clickFormAuthencation() {
         clickLink("Form Authentication");
         return new LoginPage(driver);
 
     }
-    public DropdownPage clickDropDown(){
+
+    public DropdownPage clickDropDown() {
         clickLink("Dropdown");
         return new DropdownPage(driver);
     }
-    public ForgotPasswordPage clickForgotPassword(){
+
+    public ForgotPasswordPage clickForgotPassword() {
         clickLink("Forgot Password");
         return new ForgotPasswordPage(driver);
     }
-    public  HoversPage clickHovers(){
+
+    public HoversPage clickHovers() {
         clickLink("Hovers");
         return new HoversPage(driver);
     }
-    public KeyPressesPage clickKeyPresses(){
+
+    public KeyPressesPage clickKeyPresses() {
         clickLink("Key Presses");
         return new KeyPressesPage(driver);
     }
-    public HorizontalSliderPage clickHorizontalSlider(){
+
+    public HorizontalSliderPage clickHorizontalSlider() {
         clickLink("Horizontal Slider");
         return new HorizontalSliderPage(driver);
     }
-    public AlertsPage clickAJavaScriptAlerts(){
+
+    public AlertsPage clickAJavaScriptAlerts() {
         clickLink("JavaScript Alerts");
         return new AlertsPage(driver);
     }
-    public FileUploadPage clickFileUpload(){
+
+    public FileUploadPage clickFileUpload() {
         clickLink("File Upload");
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         return new FileUploadPage(driver);
     }
 
-    public DynamicLoadingPage clickDynamicLoading(){
+    public DynamicLoadingPage clickDynamicLoading() {
         clickLink("Dynamic Loading");
         return new DynamicLoadingPage(driver);
     }
-    public ContextMenuPage clickContextMenu(){
+
+    public ContextMenuPage clickContextMenu() {
         clickLink("Context Menu");
-        driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         return new ContextMenuPage(driver);
     }
-    public WysiwygEditorPage clickWysiwygEditor(){
+
+    public WysiwygEditorPage clickWysiwygEditor() {
         clickLink("WYSIWYG Editor");
-        driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         return new WysiwygEditorPage(driver);
     }
 
-    public FramesPage clickFramesPage(){
+    public FramesPage clickFramesPage() {
         clickLink("Frames");
-        driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         return new FramesPage(driver);
     }
-    public LargeAndDeepDomPage clickLargeAndDeepDom(){
+
+    public LargeAndDeepDomPage clickLargeAndDeepDom() {
         clickLink("Large & Deep DOM");
         return new LargeAndDeepDomPage(driver);
     }
-    public InfiniteScrollPage clickInfiniteScroll(){
+
+    public InfiniteScrollPage clickInfiniteScroll() {
         clickLink("Infinite Scroll");
         return new InfiniteScrollPage(driver);
     }
-    public MultipleWindowsPage clickMultipleWindows(){
+
+    public MultipleWindowsPage clickMultipleWindows() {
         clickLink("Multiple Windows");
         return new MultipleWindowsPage(driver);
     }
--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,17 +1,27 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import utils.WindowManager;
+
+import java.io.File;
+import java.io.IOException;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
+    private TakesScreenshot camera;
+
     @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
@@ -32,6 +42,19 @@ public class BaseTests {
     }
     public WindowManager getWindowManager(){
         return new WindowManager(driver);
+    }
+    @AfterMethod
+    public void recordFailure(ITestResult result){
+        if(ITestResult.FAILURE==result.getStatus())
+        {
+            camera= (TakesScreenshot)driver;
+            File screenshot=camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("resources/screenshots/" + result.getName() +".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
